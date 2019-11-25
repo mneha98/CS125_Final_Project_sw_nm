66fbeb0c622ecf2fb2205e701abfded59b452ac7
mneha98 <nehaam2@illinois.edu>
2019-11-25 16:31:58 -0600

    commit on 11/25 Main Activity UI added Settings Activity UI added

--- app/src/main/java/com/example/cs125_final_project/MainActivity.java
+++ app/src/main/java/com/example/cs125_final_project/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.cs125_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //comment
+        ImageButton settings = findViewById(R.id.buttonSettings);
+        settings.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+            }
+        });
+
     }
 }
